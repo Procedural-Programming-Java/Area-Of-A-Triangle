1d3b3512d944681f40798c0c240f9771de36b569
JackShkifati28 <97077461+JackShkifati28@users.noreply.github.com>
2022-01-03 23:25:51 -0500

    Update HeronsFormula4.java

--- HeronsFormula4.java
+++ HeronsFormula4.java
@@ -1,19 +1,3 @@
-/*
- * @author Yaaqov Shkifati
- * @since 09/08/19
- * @version 1.0 Description: In this program our objective, is to write a Java
- * program that will compute the area of a triangle, by using Herons Formula. We
- * are given 5 different sets of data, representing lengths of two sides of a
- * triangle that we are calling lengthA and lengthB (variables). Therefore, we
- * need to use the Pythagorean- Theorem to get the length of lengthC, in order
- * to enable us to use the Herons- Formula. Once we having all the variables, we
- * will need to conduct a two-step process. The first step is by finding half
- * perimeter (S) before finding the area. The Formula is s = (a + b + c)/2. The
- * Second step is to find the area of the triangle by using this formula Area=
- * sqrt(s(s-a)(s-b)(s-c)). We will need to add 5 more additional data than what
- * was given to us in order to complete this program.
- *.
- */
 package heronsformula4;
 
 import java.util.Scanner;
